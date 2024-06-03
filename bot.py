8a4bb015c58c09886a29daedd387d54c84f1a790
Albert-1806 <beto.gamer.2408@gmail.com>
2024-06-03 17:45:56 -0500

    Update Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import discord
+import discord, random
 from discord.ext import commands
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,5 +18,25 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'Yes, {ctx.subcommand_passed} is cool')
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
 
 bot.run("TOKEN")
